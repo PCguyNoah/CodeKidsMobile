291b458437c81634d02f04dbe952fc4483938a10
Noah Beightol <noahbeightol14@gmail.com>
2023-11-05 13:31:43 -0500

    Built out login and register pages

--- lib/main.dart
+++ lib/main.dart
@@ -79,43 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void _register() async {
-    final String apiUrl = 'https://codekidz-5a5dbee745fa.herokuapp.com/api/user/signup';
-
-    final String email = usernameController.text;
-    final String password = passwordController.text;
-
-    final Map<String, String> data = {
-      'email': email,
-      'password': password,
-    };
-
-    final response = await http.post(
-      Uri.parse(apiUrl),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: json.encode(data),
-    );
-
-    if (response.statusCode == 200) {
-      // Successfully logged in, display a success message.
-      setState(() {
-        loginStatus = response.body;
-      });
-
-      // You may want to navigate to the dashboard page here.
-      Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => DashBoard(),
-      ));
-    } else {
-      // Login failed, display an error message.
-      setState(() {
-        loginStatus = response.body;
-      });
-    }
+  void _transitionRegister() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => Register(),
+    ));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -151,11 +119,23 @@ class _MyHomePageState extends State<MyHomePage> {
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: _login,
-              child: Text('Login'),
+              child: Text('Access Your Account'),
             ),
-            ElevatedButton(
-              onPressed: _register,
-              child: Text('Register'),
+            Text(
+              "Don't have an account?",
+              style: TextStyle(
+                color: Colors.black, // Customize the text color
+              ),
+            ),
+            GestureDetector(
+              onTap: _transitionRegister,
+              child: Text(
+                'Sign Up Here',
+                style: TextStyle(
+                  color: Colors.blue, // Customize the text color
+                  decoration: TextDecoration.underline, // Add underline effect
+                ),
+              ),
             ),
             Text(
               loginStatus,
@@ -169,6 +149,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
 class DashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -182,5 +163,121 @@ class DashBoard extends StatelessWidget {
     );
   }
 }
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  String registerStatus = '';
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _register() async {
+    final String apiUrl = 'https://codekidz-5a5dbee745fa.herokuapp.com/api/user/signup';
+
+    final String email = usernameController.text;
+    final String password = passwordController.text;
+
+    final Map<String, String> data = {
+      'email': email,
+      'password': password,
+    };
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(data),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        registerStatus = 'Registration successful';
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => MyApp(),
+        ));
+      });
+    } else {
+      setState(() {
+        registerStatus = 'Registration failed';
+      });
+    }
+  }
+  void _transitionLogin() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => MyApp(),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Username:'),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(
+                hintText: 'Enter your username',
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text('Password:'),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Enter your password',
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _register,
+              child: Text('Create Account'),
+            ),
+            Text(
+              "Already have an account?",
+              style: TextStyle(
+                color: Colors.black, // Customize the text color
+              ),
+            ),
+            GestureDetector(
+              onTap: _transitionLogin,
+              child: Text(
+                'Login Here',
+                style: TextStyle(
+                  color: Colors.blue, // Customize the text color
+                  decoration: TextDecoration.underline, // Add underline effect
+                ),
+              ),
+            ),
+            Text(
+              registerStatus,
+              style: TextStyle(
+                color: registerStatus == 'Registration successful' ? Colors.green : Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 
