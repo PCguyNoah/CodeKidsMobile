3b41a0b8c5f5298d1f55e7ef2a301ba92cc88d45
matheus-seabra <100260792+matheus-seabra@users.noreply.github.com>
2023-11-12 19:00:32 -0500

    Populating the Modules 1-5 Pages

--- lib/pagefive.dart
+++ lib/pagefive.dart
@@ -21,6 +21,66 @@ class _ModuleFivePageState extends State<ModuleFivePage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
+      body:SingleChildScrollView(
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: <Widget>[
+                Text('printf("Welcome to Module 5");',
+                  style: TextStyle(fontSize: 35), textAlign: TextAlign.center,),
+                Text('Learn',
+                  style: TextStyle(fontSize: 25),),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 80),
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (BuildContext context){
+                                return const ModuleFivePage();
+                              })
+                      );
+                    },
+                    child: Text('Learn',
+                      style: TextStyle(fontSize: 20.0),)),
+                Text('Practice:',
+                  style: TextStyle(fontSize: 25),),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 80),
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (BuildContext context){
+                                return const ModuleFivePage();
+                              })
+                      );
+                    },
+                    child: Text('Practice',
+                      style: TextStyle(fontSize: 20.0),)),
+                Text('Test',
+                  style: TextStyle(fontSize: 25),),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 80),
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (BuildContext context){
+                                return const ModuleFivePage();
+                              })
+                      );
+                    },
+                    child: Text('Test',
+                      style: TextStyle(fontSize: 20.0),)),
+              ]
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pagetwo.dart
+++ lib/pagetwo.dart
@@ -21,6 +21,66 @@ class _ModuleTwoPageState extends State<ModuleTwoPage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
+      body:SingleChildScrollView(
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: <Widget>[
+                Text('printf("Welcome to Module 2");',
+                  style: TextStyle(fontSize: 35), textAlign: TextAlign.center,),
+                Text('Learn',
+                  style: TextStyle(fontSize: 25),),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 80),
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (BuildContext context){
+                                return const ModuleTwoPage();
+                              })
+                      );
+                    },
+                    child: Text('Learn',
+                      style: TextStyle(fontSize: 20.0),)),
+                Text('Practice:',
+                  style: TextStyle(fontSize: 25),),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 80),
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (BuildContext context){
+                                return const ModuleTwoPage();
+                              })
+                      );
+                    },
+                    child: Text('Practice',
+                      style: TextStyle(fontSize: 20.0),)),
+                Text('Test',
+                  style: TextStyle(fontSize: 25),),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 80),
+                    ),
+                    onPressed: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (BuildContext context){
+                                return const ModuleTwoPage();
+                              })
+                      );
+                    },
+                    child: Text('Test',
+                      style: TextStyle(fontSize: 20.0),)),
+              ]
+          ),
+        ),
+      ),
     );
   }
 }
